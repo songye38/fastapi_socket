db2b6be333b4485a5ab2235710c17f293333b380
songye38 <ddev0308@gmail.com>
2026-06-07 16:04:35 +0900

    update : public url-> url

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ import os
 
 load_dotenv()  # 이거 꼭 해줘야 함
 
-DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL")  # 이건 .env에 설정하거나 Railway에 입력
+DATABASE_URL = os.getenv("DATABASE_URL")  # 이건 .env에 설정하거나 Railway에 입력
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
