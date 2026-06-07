ea55a13bc119edda4e3050337f35f7e6919e2e89
songye38 <ddev0308@gmail.com>
2026-06-07 18:49:37 +0900

    update : register_user에서 비밀번호 길이 제한

--- app/auth/service.py
+++ app/auth/service.py
@@ -4,6 +4,10 @@ from . import repository
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def register_user(db, user_data):
-    # 비밀번호 암호화
-    hashed_password = pwd_context.hash(user_data.password)
+    # 비밀번호 길이 제한 처리 (72바이트 초과 시 자르기)
+    password_to_hash = user_data.password
+    if len(password_to_hash) > 72:
+        password_to_hash = password_to_hash[:72]
+        
+    hashed_password = pwd_context.hash(password_to_hash)
     return repository.create_user(db, user_data.username, hashed_password)
